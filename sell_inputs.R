29a07442d616fa2804ad7b2af305c85f8be98dbd
Omar <takemyprocs0492@gmail.com>
2021-09-08 19:52:24 -0500

    update

---
+++ sell_inputs.R
@@ -0,0 +1,93 @@
+# An alchemist' problem - extension: inputs can be sold
+# author: Takemyprocs
+# author's profile: https://github.com/Takemyprocs
+# Loading linear programming package
+library(lpSolve)
+
+# 1. Parameters
+# 1.1. Output prices
+# p1: Potion of spectral agility price
+# p2: Potion of spectral stamina price
+# p3: Potion of spectral stamina price
+# p4: Potion of spectral intellect price
+# p5: Spiritual healing potion price
+# p6: Spectral flask of stamina price
+# p7: Spectral flask of power price
+# p8: Widowbloom price
+# p9: Vigil's torch price
+# p10: Rising glory price
+# p11: Marrowroot price
+# p12: Nightshade price
+# p13: Death blossom price
+p1 <- 140.97
+p2 <- 40.66
+p3 <- 125.48
+p4 <- 149.75
+p5 <- 18.82
+p6 <- 280.97
+p7 <- 350.96
+p8 <- 16.84
+p9 <- 9.92
+p10 <- 19.95
+p11 <- 29.99
+p12 <- 10.89
+p13 <- 3.79
+# 1.2. Input stock
+# s1: Widowbloom stock
+# s2: Vigil's torch stock
+# s3: Rising glory stock
+# s4: Marrowroot stock
+# s5: Nightshade stock
+# s6: Death blossom stock
+s1 <- 226
+s2 <- 370
+s3 <- 223
+s4 <- 127
+s5 <- 246
+s6 <- 714
+
+# 2. Setting objective function coefficients
+f.obj <- c(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13)
+
+# 3. Setting block of constraints
+# 3.1. Setting coefficients
+f.con <- matrix(c(5, 0, 0, 0, 0, 0, 4, 1, 0, 0, 0, 0, 0,
+                  0, 5, 0, 0, 0, 0, 4, 0, 1, 0, 0, 0, 0,
+                  0, 0, 5, 0, 0, 3, 4, 0, 0, 1, 0, 0, 0,
+                  0, 0, 0, 5, 0, 3, 4, 0, 0, 0, 1, 0, 0,
+                  0, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 1, 0,
+                  0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1), nrow = 6, byrow = TRUE)
+# 3.2. Setting inequality signs
+f.dir <- c("<=",
+           "<=",
+           "<=",
+           "<=",
+           "<=",
+           "<=")
+# 3.3. Setting right hand side coefficients
+f.rhs <- c(s1,
+           s2,
+           s3,
+           s4,
+           s5,
+           s6)
+
+# Output
+output <- lp("max", f.obj, f.con, f.dir, f.rhs, int.vec = 1:13, all.int = TRUE)
+text <- c("agility_potion",
+          "stamina_potion",
+          "strengh_potion",
+          "intellect_potion",
+          "healing_potion",
+          "stamina_flask",
+          "power_flask",
+          "widowbloom",
+          "vigil_torch",
+          "rising_glory",
+          "marrowroot",
+          "nightshade",
+          "death_blossom")
+table <- data.frame(cbind(text,output$solution))
+colnames(table) <- c("item","optimal_sell")
+print(paste("Optimal profit is:",output$objval, "gold coins"))
+print(table)
--- seller.R
+++ seller.R
@@ -1,69 +1,67 @@
-# --------------------- #
-# Alchemist's Problem 1 #
-# --------------------- #
+# An alchemist' problem
+# author: Takemyprocs
+# author's profile: https://github.com/Takemyprocs
+# Loading linear programming package
+library(lpSolve)
 
-# parámetros
-# precio de outputs
-# p1: precio de poción de agilidad
-# p2: precio de poción de aguante espectral
-# p3: precio de poción de fuerza espectral
-# p4: precio de poción de intelecto espectral
-# p5: precio de frasco de aguante espectral
-# p6: precio de frasco de poder espectral
+# 1. Parameters
+# 1.1. Output prices
+# p1: Potion of spectral agility price
+# p2: Potion of spectral stamina price
+# p3: Potion of spectral stamina price
+# p4: Potion of spectral intellect price
+# p5: Spectral flask of stamina price
+# p6: Spectral flask of power price
 p1 <- 100.2
 p2 <- 80
 p3 <- 110.96
 p4 <- 120.11
 p5 <- 115.90
 p6 <- 314
-# stock de insumos
-# s1: stock de viudalia
-# s2: stock de antorcha de la vigilia
-# s3: stock de gloria creciente
-# s4: stock de corraiz
-# s5: stock de sombranoche
+# 1.2. Input costs
+# r1: Widowbloom price
+# r2: Vigil's torch price
+# r3: Rising glory price
+# r4: Marrowroot price
+# r5: Nightshade price
 s1 <- 98
 s2 <- 147
 s3 <- 182
 s4 <- 157
 s5 <- 114
-# coeficientes asociados a variables de decisión (x6)
-# c1 <- producción de poción de agilidad
-# c2 <- producción de poción de aguante
-# c3 <- producción de poción de fuerza
-# c4 <- producción de poción de intelecto
-# c5 <- producción de frasco de aguante
-# c6 <- producción de frasco de poder
 
-# Package de programación lineal
-library(lpSolve)
-# Estableciendo coeficientes de la función objetivo
+# 2. Setting objective function coefficients
 f.obj <- c(p1, p2, p3, p4, p5, p6)
 
-# Set matrix corresponding to coefficients of constraints by rows
-# Do not consider the non-negative constraint; it is automatically assumed
+# 3. Setting block of constraints
+# 3.1. Setting coefficients
 f.con <- matrix(c(5, 0, 0, 0, 0, 4,
                   0, 5, 0, 0, 0, 4,
                   0, 0, 5, 0, 3, 4,
                   0, 0, 0, 5, 3, 4,
                   0, 0, 0, 0, 1, 3), nrow = 5, byrow = TRUE)
-
-# Set unequality signs
+# 3.2. Setting inequality signs
 f.dir <- c("<=",
            "<=",
            "<=",
            "<=",
            "<=")
-
-# Set right hand side coefficients
+# 3.3. Setting right hand side coefficients
 f.rhs <- c(s1,
            s2,
            s3,
            s4,
            s5)
 
-# Final value (z)
-output <- lp("max", f.obj, f.con, f.dir, f.rhs, int.vec = 1:6, all.int = TRUE)$solution
-text <- c("x1","x2","x3","x4","x5","x6")
-output <- cbind(text,output)
-output
+# Output
+output <- lp("max", f.obj, f.con, f.dir, f.rhs, int.vec = 1:6, all.int = TRUE)
+text <- c("agility_potion_sell",
+          "stamina_potion_sell",
+          "strengh_potion_sell",
+          "intellect_potion_sell",
+          "stamina_flask_sell",
+          "power_flask_sell")
+table <- data.frame(cbind(text,output$solution))
+colnames(table) <- c("item","optimal_sell")
+print(paste("Optimal profit is:",output$objval, "gold coins"))
+print(table)
